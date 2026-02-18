6f85db0be6680920d157600d9f7b1b9ea3fb423c
devanand7 <devanandutkarsh7@gmail.com>
2026-02-18 11:20:07 +0530

    here i modify the server , because when i test it its not send thee request to server to updifyed the cors, i will fix in productions

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import rateLimit from 'express-rate-limit';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // Import routes
 import authRoutes from './routes/auth.js';
@@ -29,10 +31,7 @@ const limiter = rateLimit({
 
 // Security & Middleware
 app.use(helmet());
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  credentials: true
-}));
+app.use(cors());
 app.use(limiter); // Apply rate limiting to all routes
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
